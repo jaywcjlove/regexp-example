3aa35fc6c1c44c9afa5bd7c5e135eaf9ed1d2e9f
jaywcjlove <398188662@qq.com>
2021-11-15 16:31:25 +0800

    Add Document.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# regex-example
+RegExp Example
+===
+
+正则表达式实例搜集，通过实例来学习正则表达式。本仓库实例搜集整理来自于[**《一些正则表达式随记》**](https://github.com/jaywcjlove/handbook/blob/master/Javascript/%E4%B8%80%E4%BA%9B%E6%AD%A3%E5%88%99%E8%A1%A8%E8%BE%BE%E5%BC%8F%E9%9A%8F%E8%AE%B0.md)，通过一个单独仓库专门整理这些正则实例，提供一个网站，方便正则实例验证和查询。也方便你[**添加**](https://github.com/jaywcjlove/regexp-example/issues/new?labels=new&assignees=jaywcjlove&title=%E6%B7%BB%E5%8A%A0%E6%96%B0%E5%AE%9E%E4%BE%8B%EF%BC%9A)新的正则实例。
+
+### 用户名
+
+验证 **数字**、**字母**、**_**、**-**，不包含特殊字符，长度 `4-16` 之间。
+
+```regex
+^[a-zA-Z0-9_-]{4,16}$
+```
+
+### 密码强度
+
+必须是包含大小写**字母**和**数字**的组合，不能使用**特殊字符**，长度在 `8-10` 之间。
+
+```regex
+^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,10}$
+```
+
+### 火车车次
+
+```regex
+^[GCDZTSPKXLY1-9]\d{1,4}$
+```
+
+E.g: `G2868`, `D22`, `D9`, `Z5`, `Z24`, `Z17`
+
+### 校验中文
+
+```regex
+^[\u4e00-\u9fa5]{0,}$
+```
+
+E.g: `中文`, `湖北`, `黄冈`
---
+++ scripts/build.mjs
@@ -0,0 +1,124 @@
+import FS from 'fs-extra';
+import path from 'path';
+import { create } from 'markdown-to-html-cli';
+
+const deployDir = path.resolve(process.cwd(), 'web');
+const mdPath = path.resolve(process.cwd(), 'README.md');
+const htmlPath = path.resolve(process.cwd(), 'web', 'index.html');
+
+const style = `
+span.charset { background: #fff9c0; }
+span.range { border: 1px dotted; border-color: transparent;}
+span.range:hover { border: 1px dotted #0037f9; }
+div.regex { margin-top: 10px; display: flex; justify-content: space-between; }
+div.regex input {
+  outline: none;
+  border: none;
+  padding: 4px 6px;
+  border-radius: 3px;
+  box-sizing: border-box;
+  flex: 1;
+}
+div.regex input:hover { box-shadow: 0 0 0 1px #ffffff, 0 0 0 3px rgb(55 109 217 / 21%), inset 0 1px 1px rgb(16 22 26 / 0%); }
+div.regex span.success {
+  color: #00ad36;
+  background-color: #cef3cf;
+  padding: 3px 4px;
+  border-radius: 2px;
+  margin-left: 10px;
+}
+div.regex span.danger {
+  color: #cb0649;
+  background-color: #fbdcdc;
+  padding: 3px 4px;
+  border-radius: 2px;
+  margin-left: 10px;
+}
+`;
+
+const script = `
+Array.from(document.getElementsByTagName('input')).forEach((elm) => {
+  const code = (elm.dataset.code || '').replace(/\\n/g, '');
+  elm.oninput = (evn) => {
+    const isChecked = new RegExp(code).test(evn.target.value);
+    if (elm.nextSibling) {
+      elm.nextSibling.innerHTML = isChecked ? '通过' : '不通过'
+      elm.nextSibling.className = isChecked ? 'success' : 'danger';
+    }
+  }
+});
+`;
+
+const getRegCode = (arr = []) => arr.map(item => {
+  if (item.type === 'text') {
+    return item.value;
+  }
+  if (item.children) {
+    return getRegCode(item.children);
+  }
+  return item
+}).filter(Boolean).flat().join('');
+
+const input = (code) => {
+  return {
+    type: 'element',
+    tagName: 'div',
+    properties: { className: 'regex', },
+    children: [
+      {
+        type: 'element',
+        tagName: 'input',
+        properties: {
+          type: 'text', value: '', placeholder: 'E.g: 请输入字符串验证', 'data-code': code || '',
+        },
+      },
+      {
+        type: 'element',
+        tagName: 'span',
+        properties: { className: 'info' },
+      }
+    ]
+  }
+}
+
+const options = {
+  'github-corners': 'https://github.com/jaywcjlove/regexp-example.git',
+  document: {
+    style, script,
+    link: [
+      // { rel: 'shortcut icon', href: './favicon.ico' },
+    ]
+  },
+  rewrite: (node, index, parent) => {
+    if (node.tagName === 'pre' && node.properties.className) {
+      const lang = Array.isArray(node.properties.className) ? node.properties.className.join('') : node.properties.className;
+      if (/-regex$/.test(lang)) {
+        const regCode = node.children[0];
+        const regStr = getRegCode(regCode.children);
+        console.log('parent:', parent)
+        console.log('node:', node)
+        console.log('index:', parent[index-1])
+        node.children = [ ...node.children, input(regStr) ];
+      }
+    }
+  }
+}
+
+;(async () => {
+  await FS.ensureDir(deployDir);
+  await FS.emptyDir(deployDir);
+  const mdStr = (await FS.readFile(mdPath)).toString();
+  const html = await create({
+    ...options,
+    markdown: mdStr,
+    document: {
+      title: "RegExp Example",
+      ...options.document,
+      meta: [
+        { description: 'RegExp Example.' },
+        { keywords: 'RegExp,example' }
+      ]
+    }
+  });
+  await FS.writeFile(htmlPath, html);
+})();
