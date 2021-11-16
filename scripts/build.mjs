5e0ca8a9093178b9403dd25a4b3f35060eb11362
jaywcjlove <398188662@qq.com>
2021-11-16 17:57:11 +0800

    chore: update build scripts.

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,30 +10,67 @@ const style = `
 span.charset { background: #fff9c0; }
 span.range { border: 1px dotted; border-color: transparent;}
 span.range:hover { border: 1px dotted #0037f9; }
-div.regex { margin-top: 10px; display: flex; justify-content: space-between; }
+div.regex { margin-top: 10px; display: flex; align-items: center; position: relative; }
 div.regex input {
   outline: none;
   border: none;
   padding: 4px 6px;
+  padding-right: 60px;
   border-radius: 3px;
   box-sizing: border-box;
+  line-height: 18px;
   flex: 1;
 }
-div.regex input.danger { box-shadow: 0 0 0 1px #ffffff, 0 0 0 3px rgb(253 104 155), inset 0 1px 1px rgb(16 22 26 / 0%) !important; }
+div.regex input.success { box-shadow: 0 0 0 1px #ffffff, 0 0 0 3px #15ae3c, inset 0 1px 1px rgb(16 22 26 / 0%) !important; }
+div.regex input.danger { box-shadow: 0 0 0 1px #ffffff, 0 0 0 3px #e91e63, inset 0 1px 1px rgb(16 22 26 / 0%) !important; }
 div.regex input:hover { box-shadow: 0 0 0 1px #ffffff, 0 0 0 3px rgb(55 109 217 / 21%), inset 0 1px 1px rgb(16 22 26 / 0%); }
+div.regex span.success,
+div.regex span.danger { position: absolute; right: 1px; padding: 2px 3px; border-radius: 2px; }
 div.regex span.success {
   color: #00ad36;
   background-color: #cef3cf;
-  padding: 3px 4px;
-  border-radius: 2px;
   margin-left: 10px;
 }
 div.regex span.danger {
   color: #cb0649;
   background-color: #fbdcdc;
-  padding: 3px 4px;
-  border-radius: 2px;
   margin-left: 10px;
+}
+
+.markdown-body pre {
+  position: relative;
+}
+.markdown-body pre[class*="language-regex"] {
+  overflow: initial;
+}
+.markdown-body pre[class*="language-regex"]:hover .issue {
+  visibility: visible;
+}
+.markdown-body pre[class*="language-regex"] .issue {
+  visibility: hidden;
+  position: absolute;
+  right: 3px;
+  margin-top: -28px;
+  font-size: 12px;
+}
+.markdown-body pre[class*="language-regex"] .issue a {
+  background: #ff5722;
+  padding: 3px 5px;
+  border-radius: 2px;
+  color: #fff;
+}
+.markdown-body pre[class*="language-regex"] .issue a + a {
+  margin-left: 5px;
+}
+.markdown-body pre[class*="language-regex"] .issue a.copy {
+  background: #2196f3;
+}
+.markdown-body pre[class*="language-regex"] .issue a.copied {
+  background: #4caf50;
+}
+.markdown-body pre > code[class*="language-regex"] {
+  word-break: break-all !important;
+  white-space: initial !important;
 }
 `;
 
@@ -49,6 +86,17 @@ Array.from(document.getElementsByTagName('input')).forEach((elm) => {
     }
   }
 });
+
+function copied(target, str = '') {
+  target.classList.add('copied')
+  target.innerHTML = '复制成功!';
+  copyTextToClipboard(str, function() {
+    setTimeout(() => {
+      target.innerHTML = '点击复制';
+      target.classList.remove('copied')
+    }, 2000);
+  });
+}
 `;
 
 const getRegCode = (arr = []) => arr.map(item => {
@@ -71,7 +119,7 @@ const input = (code) => {
         type: 'element',
         tagName: 'input',
         properties: {
-          type: 'text', value: '', placeholder: 'E.g: 请输入字符串验证', 'data-code': code || '',
+          type: 'text', value: '', placeholder: '请输入下方【E.g】字符串验证实例', 'data-code': code || '',
         },
       },
       {
@@ -83,10 +131,50 @@ const input = (code) => {
   }
 }
 
+const toolbar = (copied) => {
+  return {
+    type: 'element',
+    tagName: 'div',
+    properties: { className: 'issue' },
+    children: [
+      {
+        type: 'element',
+        tagName: 'a',
+        properties: {
+          className: 'copy',
+          onclick: `copied(this, "${copied}")`,
+          href: `javascript: this;`
+        },
+        children: [
+          {
+            type: 'text',
+            value: '点击复制'
+          }
+        ]
+      },
+      {
+        type: 'element',
+        tagName: 'a',
+        properties: {
+          target: '__blank',
+          href: `https://github.com/jaywcjlove/regexp-example/issues/new?labels=bug,enhancement&assignees=jaywcjlove&body=❌正则：~~\`${copied}\`~~&title=修改实例：xxx`
+        },
+        children: [
+          {
+            type: 'text',
+            value: '🐞修改正则'
+          }
+        ]
+      }
+    ],
+  }
+}
+
 const options = {
   'github-corners': 'https://github.com/jaywcjlove/regexp-example.git',
   document: {
     style, script,
+    js: 'https://unpkg.com/@uiw/copy-to-clipboard/dist/copy-to-clipboard.umd.js',
     link: [
       // { rel: 'shortcut icon', href: './favicon.ico' },
     ]
@@ -96,11 +184,11 @@ const options = {
       const lang = Array.isArray(node.properties.className) ? node.properties.className.join('') : node.properties.className;
       if (/-regex$/.test(lang)) {
         const regCode = node.children[0];
-        const regStr = getRegCode(regCode.children);
-        console.log('parent:', parent)
-        console.log('node:', node)
-        console.log('index:', parent[index-1])
-        node.children = [ ...node.children, input(regStr) ];
+        const regStr = getRegCode(regCode.children).replace(/\n/, '');
+        // console.log('parent:', parent)
+        // console.log('node:', node)
+        // console.log('index:', parent[index-1])
+        node.children = [ toolbar(regStr), ...node.children, input(regStr) ];
       }
     }
   }
